ba94f512ca9d696dcc4264090dd137340ce21f4b
Ravi <pandeyravi170@gmail.com>
2026-06-17 15:19:14 +0530

    Added all Vercel domains to CORS

--- app/main.py
+++ app/main.py
@@ -32,6 +32,7 @@ origins = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "https://statsathi-frontend.vercel.app"
+    "https://statsathi-frontend-ravi-statsathi.vercel.app"
 ]
 
 app.add_middleware(
